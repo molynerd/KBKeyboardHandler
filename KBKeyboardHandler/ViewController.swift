9700b3466d99dfe537d8523b7b7b87fa7b8162df
nmolyneux <nickneux21@gmail.com>
2015-07-28 17:56:51 -0700

    Initial Commit

---
+++ KBKeyboardHandler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KBKeyboardHandler
+//
+//  Created by Nicholas Molyneux on 7/28/15.
+//  Copyright © 2015 Nicholas Molyneux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KBKeyboardHandlerTests/KBKeyboardHandlerTests.swift
@@ -0,0 +1,36 @@
+//
+//  KBKeyboardHandlerTests.swift
+//  KBKeyboardHandlerTests
+//
+//  Created by Nicholas Molyneux on 7/28/15.
+//  Copyright © 2015 Nicholas Molyneux. All rights reserved.
+//
+
+import XCTest
+@testable import KBKeyboardHandler
+
+class KBKeyboardHandlerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
